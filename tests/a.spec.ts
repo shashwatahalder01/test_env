4c2510cdc05ea038a272c066e8e1b29f3c908f07
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-05-13 09:49:46 +0600

    fixed tests

--- tests/a.spec.ts
+++ tests/a.spec.ts
@@ -1,26 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-test('test1', async ({ page }) => {
+test('test1', async ( ) => {
   expect(true).toBeTruthy();
 });
 
-test('test2', async ({ page }) => {
+test('test2', async ( ) => {
   expect(true).toBeTruthy();
 });
 
-test('test3', async ({ page }) => {
+test('test3', async ( ) => {
   expect(false).toBeTruthy();
 });
 
-test('test4', async ({ page }) => {
+test('test4', async ( ) => {
   expect(false).toBeTruthy();
 });
 
-test('test5', async ({ page }) => {
+test('test5', async ( ) => {
   test.skip();
 });
 
-test('test6', async ({ page }) => {
+test('test6', async ( ) => {
   test.skip();
 });
 
