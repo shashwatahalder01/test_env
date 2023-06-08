6147ae4d092593f948b666062364749be6a4997b
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-06-08 16:00:48 +0600

    testing report

---
+++ tests/api/b.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('test_b_1 ', async ( ) => {
+  expect(true).toBeTruthy();
+});
+
+test('test_b_2', async ( ) => {
+  expect(true).toBeTruthy();
+});
+
+// test('test3', async ( ) => {
+//   expect(false).toBeTruthy();
+// });
+
+// test('test4', async ( ) => {
+//   expect(false).toBeTruthy();
+// });
+
+// test('test5', async ( ) => {
+//   test.skip();
+// });
+
+// test('test6', async ( ) => {
+//   test.skip();
+// });
+
