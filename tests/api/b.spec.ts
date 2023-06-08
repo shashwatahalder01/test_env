58236015e39f09d8eda14647dd899017e30a2ce5
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-06-08 22:55:50 +0600

    reporter

--- tests/api/b.spec.ts
+++ tests/api/b.spec.ts
@@ -8,19 +8,19 @@ test('test_b_2', async ( ) => {
   expect(true).toBeTruthy();
 });
 
-// test('test3', async ( ) => {
-//   expect(false).toBeTruthy();
-// });
+test('test3', async ( ) => {
+  expect(false).toBeTruthy();
+});
 
-// test('test4', async ( ) => {
-//   expect(false).toBeTruthy();
-// });
+test('test4', async ( ) => {
+  expect(false).toBeTruthy();
+});
 
-// test('test5', async ( ) => {
-//   test.skip();
-// });
+test('test5', async ( ) => {
+  test.skip();
+});
 
-// test('test6', async ( ) => {
-//   test.skip();
-// });
+test('test6', async ( ) => {
+  test.skip();
+});
 
