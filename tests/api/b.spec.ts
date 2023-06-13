e44e86aebdf1ff05f4e4cac9242042547f1e740f
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-06-13 18:07:04 +0600

    q

--- tests/api/b.spec.ts
+++ tests/api/b.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('test_b_1 ', async () => {
-  process.env.ABC = 'abc abc abc abc abc abc';
   expect(true).toBeTruthy();
 });
 
@@ -10,15 +9,13 @@ test('test_b_2', async ( ) => {
 
 });
 
-test('test3', async ( ) => {
-  expect(false).toBeTruthy();
-  console.log(process.env.ABC);
-  console.log(process.env.QWERTY);
-});
+// test('test3', async ( ) => {
+//   expect(false).toBeTruthy();
+// });
 
-test('test4', async ( ) => {
-  expect(false).toBeTruthy();
-});
+// test('test4', async ( ) => {
+//   expect(false).toBeTruthy();
+// });
 
 test('test5', async ( ) => {
   test.skip();
