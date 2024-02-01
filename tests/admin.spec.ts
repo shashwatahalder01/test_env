0676672860fd42547ea79c83399a47d47fc37406
shashwata <shashwatahalder01@gmail.com>
2024-02-01 15:32:08 +0600

    update tests

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect, Page } from "@playwright/test";
 
+
 test.describe("Admin user functionality test", () => {
   test("Plugin found in plugin list", async ({ page }) => {
-    await page.goto("http://dokan1.test" + "/wp-login.php");
+    await page.goto('http://localhost:8889' + "/wp-login.php");
     await page.getByLabel("Username or Email Address").fill("admin");
     await page.waitForTimeout(1000);
-    await page.getByLabel("Password", { exact: true }).fill("01dokan01");
+    await page.getByLabel("Password", { exact: true }).fill("password");
     await page.getByText("Log In").click();
-    await page.goto("http://dokan1.test/wp-admin/");
+    await page.goto("http://localhost:8889/wp-admin/");
     await page.locator("a.menu-icon-plugins").click();
-    await page.screenshot({ path: "playwrite/screenshot.png", fullPage: true });
+    await page.screenshot({ path: 'playwrite/screenshot.png', fullPage: true })
   });
 });
