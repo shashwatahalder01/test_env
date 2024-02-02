a22b15724583a992a36e8188cdb84ca7f629d7dd
shashwata <shashwatahalder01@gmail.com>
2024-02-02 08:43:06 +0600

    update test

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -1,15 +1,19 @@
 import { test, expect, Page } from "@playwright/test";
 
+// const url = "http://dokan1.test";
+const url = 'http://localhost:8889';
+// const password = '01dokan01';
+const password = 'password';
 
 test.describe("Admin user functionality test", () => {
   test("Plugin found in plugin list", async ({ page }) => {
-    await page.goto('http://localhost:8889' + "/wp-login.php");
+    await page.goto(url + "/wp-login.php");
     await page.getByLabel("Username or Email Address").fill("admin");
     await page.waitForTimeout(1000);
-    await page.getByLabel("Password", { exact: true }).fill("password");
+    await page.getByLabel("Password", { exact: true }).fill(password);
     await page.getByText("Log In").click();
-    await page.goto("http://localhost:8889/wp-admin/");
+    await page.goto(url + "/wp-admin/");
     await page.locator("a.menu-icon-plugins").click();
-    await page.screenshot({ path: 'playwrite/screenshot.png', fullPage: true })
+    await page.screenshot({ path: "playwright/screenshot.png", fullPage: true });
   });
 });
